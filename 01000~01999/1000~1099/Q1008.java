a58aec2db3d195c3ea14d66a2848036f5243739d
KIMSEI <bbomi1973@gmail.com>
2022-01-05 14:53:31 +0900

    Q1008(Solve)
    , Q1065(Solving) code update

--- 01000~01999/1000~1099/Q1008.java
+++ 01000~01999/1000~1099/Q1008.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Q1008 {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        double c = a/b;
-        double d = a%b;
-        System.out.println(c +" "+ d);
+        double b = sc.nextInt();
+        System.out.println(a/b);
+
     }
 }
