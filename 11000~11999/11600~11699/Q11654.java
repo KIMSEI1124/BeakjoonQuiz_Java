b318ee6fa8cad095afac55a837c6f09943856546
KIMSEI <bbomi1973@gmail.com>
2022-01-27 17:09:57 +0900

    Q11654 solve

---
+++ 11000~11999/11600~11699/Q11654.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q11654 {
+    static Scanner sc = new Scanner(System.in);
+    static String str = sc.next();
+    public static void main(String[] args){
+        solve();
+    }
+
+    public static void solve() {
+        char c = str.charAt(0);
+        int num = (int) c;
+        System.out.println(num);
+    }
+}
