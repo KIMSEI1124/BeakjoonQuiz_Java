62c48604eb1d996fc5c539db7e4bb55d65d83760
KIMSEI <bbomi1973@gmail.com>
2022-02-06 10:43:53 +0900

    Q18108 solve

---
+++ 18000~18999/18100~18199/Q18108.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Q18108 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        int y = sc.nextInt()-543;
+        System.out.println(y);
+    }
+}
