1234a7c81e523d63708849e11a3eb6a624aeb556
KIMSEI <bbomi1973@gmail.com>
2022-02-25 10:10:08 +0900

    Q10951 solve

--- 10000~10999/10900~10999/Q10951.java
+++ 10000~10999/10900~10999/Q10951.java
@@ -1,29 +1,18 @@
+import java.io.*;
 import java.util.*;
 
 public class Q10951 {
-    static Scanner sc = new Scanner("");
-    static int num1, num2;
-
-    public static void main(String[] args) {
-        solve();
-    }
-
-    public static void input() {
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-    }
-
-    public static void solve() {
-        while (true) {
-            try {
-                if (sc.hasNext() == true) {
-                    input();
-                }
-            } catch (NoSuchElementException e) {
-                break;
-            } finally {
-                System.out.println(num1 + num2);
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+        while ((str = br.readLine()) != null && str.length() > 0) {
+            st = new StringTokenizer(str);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b + "\n");
         }
+        System.out.println(sb);
     }
 }
