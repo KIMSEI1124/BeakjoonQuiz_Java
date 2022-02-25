582bf045e277e70f354858f6c820c6323c8b9840
KIMSEI <bbomi1973@gmail.com>
2022-02-25 10:30:14 +0900

    Q4153 solve

--- 01000~01999/1100~1199/Q1157.java
+++ 01000~01999/1100~1199/Q1157.java
@@ -1,19 +1,10 @@
+import java.io.*;
 import java.util.*;
 public class Q1157 {
-    static Scanner sc = new Scanner(System.in);
-    static String str = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().toUpperCase().split("");
+        String[][] arr2 = {{"A","0"},{"B","0"},{"C","0"}};
 
-    public static void main(String[] args) {
-        HashMap<String, Integer> hash = new HashMap<>();
-        str.toUpperCase();
-        String[] arr = str.split("");
-        for (int i = 0; i < arr.length; i++) {
-            if (hash.containsKey(arr[i]) == true) {
-                int count = hash.get(arr[i]) + 1;
-                hash.replace(arr[i], count);
-            } else {
-                hash.put(arr[i], 1);
-            }
-        }
     }
 }
