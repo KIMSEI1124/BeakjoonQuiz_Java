6a27883e54af6ba1635ddef408b4ea32294a3f53
KIMSEI <bbomi1973@gmail.com>
2022-02-27 19:22:18 +0900

    Q10989 solve

--- 10000~10999/10900~10999/Q10989.java
+++ 10000~10999/10900~10999/Q10989.java
@@ -1,32 +1,43 @@
+import java.io.*;
 import java.util.*;
-// 시간 초과 BufferReader 사용 해야할듯..
-
-@SuppressWarnings({ "rawtypes", "unchecked" })
 
 public class Q10989 {
-    static Scanner sc = new Scanner(System.in);
-    static int N = sc.nextInt();
-
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        input(list);
-        solve(list);
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        solve2();
     }
 
-    public static ArrayList input(ArrayList list) {
+    public static void solve1() throws NumberFormatException, IOException { // 메모리 418192KB, 시간 : 2624ms
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            ans.append(arr[i] + "\n");
+        }
+        System.out.println(ans);
+    }
+
+    public static void solve2() throws NumberFormatException, IOException { // 메모리 385036KB, 시간 : 1748ms
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10000];
         while (N > 0) {
-            list.add(sc.nextInt());
+            int num = Integer.parseInt(br.readLine());
+            arr[num - 1] += 1;
             N--;
         }
-        return list;
-    }
-
-    public static void solve(ArrayList list) {
-        StringBuilder sb = new StringBuilder();
-        Collections.sort(list);
-        for(int i = 0; i < list.size(); i++) {
-            sb.append(list.get(i)+"\n");
+        for (int i = 0; i < 10000; i++) {
+            int count = arr[i];
+            while (count > 0) {
+                ans.append((i + 1) + "\n");
+                count--;
+            }
         }
-        System.out.println(sb);
+        System.out.println(ans);
     }
 }
