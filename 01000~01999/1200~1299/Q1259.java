bcc05fe6f3e250d40394ce183a8be55525fc1c1f
KIMSEI <bbomi1973@gmail.com>
2022-02-27 18:47:04 +0900

    Q1259 solve

--- 01000~01999/1200~1299/Q1259.java
+++ 01000~01999/1200~1299/Q1259.java
@@ -6,20 +6,21 @@ public class Q1259 {
     public static void main(String[] args) throws IOException {
         StringBuilder ans = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str;
         while (true) {
-            str = br.readLine();
-            if (str.equals("0")) {
+            String[] arr = br.readLine().split("");
+            if (arr.length == 1 && arr[0].equals("0")) { // 배열의 크기가 1이고 값이 0이면 종료
                 break;
-            }
-            String[] arr = str.split("");
-            for (int i = 1; i < arr.length / 2; i++) {
-                if (arr.length % 2 == 0 || !arr[i].equals(arr[arr.length - i])) {
-                    ans.append("no\n");
-                    break;
-                }
-                if (i - 1 == arr.length / 2) {
-                    ans.append("yes\n");
+            } else if (arr.length == 1 && !arr[0].equals("0")) { // 배열의 크기가 1이면 팰린드롬수이므로 yes
+                ans.append("yes\n");
+            } else { // 배열의 크기가 홀수 이며 3 이상일 경우 확인
+                for (int i = 0; i < arr.length / 2; i++) {
+                    if (!arr[i].equals(arr[arr.length - 1 - i])) { // 값 확인시 다르면 no
+                        ans.append("no\n");
+                        break;
+                    }
+                    if (arr.length / 2 - 1 == i) { // 마지막까지 정상이면 팰린드롬수
+                        ans.append("yes\n");
+                    }
                 }
             }
         }
