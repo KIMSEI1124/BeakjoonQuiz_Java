9c147314b3b3b2b870978f80f9f345a999ae539f
KIMSEI <bbomi1973@gmail.com>
2022-03-09 13:33:02 +0900

    Q2775 solve

--- 02000~02999/2700~2799/Q2775.java
+++ 02000~02999/2700~2799/Q2775.java
@@ -1,28 +1,40 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Q2775 {
     public static void main(String[] args) throws NumberFormatException, IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int[][] arr = new int[15][14];
-        int K; // 층
-        int N; // 호
-        while (T > 0) {
-            K = Integer.parseInt(br.readLine());
-            N = Integer.parseInt(br.readLine());
-            for (int i = 0; i < K + 1; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (i == 0) {
-                        arr[i][j] = j + 1;
-                    } else {
-                        
+        int[][] address = new int[T][2]; // 층과 호수 저장
+        int maxK = 0;
+        int maxN = 0;
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j <= 1; j++) {
+                address[i][j] = Integer.parseInt(br.readLine());
+                if (j == 0 && address[i][j] > maxK) {
+                    maxK = address[i][j];
+                } else if (j == 1 && address[i][j] > maxN) {
+                    maxN = address[i][j];
+                }
+            }
+        }
+        int[][] apt = new int[maxK + 1][maxN];
+        for (int i = 0; i < apt.length; i++) {
+            for (int j = 0; j < apt[i].length; j++) {
+                if (i == 0) {
+                    apt[i][j] = j + 1;
+                } else {
+                    for (int k = 0; k <= j; k++) {
+                        apt[i][j] += apt[i - 1][k];
                     }
                 }
             }
-            T--;
         }
+        for (int i = 0; i < T; i++) {
+            int k = address[i][0];
+            int n = address[i][1];
+            sb.append(apt[k][n - 1]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
