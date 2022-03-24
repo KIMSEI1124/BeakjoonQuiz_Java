9122ae442c1cd7bd46ffb090729daecbfbbac7d7
KIMSEI <bbomi1973@gmail.com>
2022-03-24 16:06:02 +0900

    Q16394 solve

---
+++ 16000~16999/16300~16399/Q16394.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Q16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt() - 1946;
+        System.out.println(N);
+    }
+}
