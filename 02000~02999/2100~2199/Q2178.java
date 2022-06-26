eb4433b134e2c40cf27f38f8ef79ac7a46fba5db
KIMSEI <bbomi1973@gmail.com>
2022-06-26 14:45:48 +0900

    README.md polishing, Q2178 solving

--- 02000~02999/2100~2199/Q2178.java
+++ 02000~02999/2100~2199/Q2178.java
@@ -3,38 +3,70 @@ import java.util.*;
 
 public class Q2178 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder ans = new StringBuilder();
     static StringTokenizer st;
-    static char[][] graph;
+
+    static int N, M;
+    static char[][] maze;
+    static boolean[][] visted;
+    static Queue<pos<Integer, Integer>> maze_pos;
 
     public static void main(String[] args) throws IOException {
         // input
+        int ans = 0;
         st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        graph = new char[N][M];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        maze = new char[N + 1][M + 1];
+        visted = new boolean[N + 1][M + 1];
+        maze_pos = new LinkedList<>();
+
         for (int i = 0; i < N; i++) {
-            graph[i] = br.readLine().toCharArray();
+            maze[i] = br.readLine().toCharArray();
+            // system.out.println("i: \"" + i + "\" > " + Arrays.toString(maze[i]) + "\"");
         }
+
         // solve
-        
+        pos<Integer, Integer> start_pos = new pos<>();
+        start_pos.set_pos(0, 0);
+        maze_pos.add(start_pos);
+        visted[start_pos.get_y_pos()][start_pos.get_x_pos()] = true;
+        while (true) {
+            int x_pos = maze_pos.peek().get_x_pos();
+            int y_pos = maze_pos.poll().get_y_pos();
+            if (y_pos == N && x_pos == M) {
+                break;
+            }
+        }
+
         // output
         System.out.println(ans);
     }
 
-    public static void up_search() {
+    static int[] x = { 0, 0, -1, 1 };
+    static int[] y = { -1, 1, 0, 0 };
 
+    static public void bfs(int y, int x) {
+        if (visted[y][x] == true) {
+            return;
+        }
+    }
+}
+
+class pos<x, y> {
+    private int x;
+    private int y;
+
+    public void set_pos(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static void down_search() {
-
-    }
-    
-    public static void left_search() {
-
+    public int get_x_pos() {
+        return this.x;
     }
 
-    public static void right_search() {
-
+    public int get_y_pos() {
+        return this.y;
     }
 }
