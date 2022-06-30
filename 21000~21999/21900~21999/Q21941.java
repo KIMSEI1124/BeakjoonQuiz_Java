c6c3a649bb6b87c4201eee41851dc11d466bd40e
KIMSEI <bbomi1973@gmail.com>
2022-06-30 13:13:21 +0900

    Q21941 failed

--- 21000~21999/21900~21999/Q21941.java
+++ 21000~21999/21900~21999/Q21941.java
@@ -5,24 +5,51 @@ public class Q21941 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
+    static String S;
+    static int M;
+
     public static void main(String[] args) throws IOException {
         // input
         int ans = 0;
-        StringBuilder sb = new StringBuilder(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        S = br.readLine();
+        M = Integer.parseInt(br.readLine());
+        String A;
+        int V;
+
         // solve
-        while (M > 0) {
-            // 문자열 입력
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            String a = st.nextToken();
-            int x = Integer.parseInt(st.nextToken());
+            A = st.nextToken(); // 문자열
+            V = Integer.parseInt(st.nextToken());   // 점수
+            S = S.replaceAll(A, "-");   // 변환
 
-            // 원래 문자열 - 이후 문자열 = 사라진 문자의 개수
-
-            M--;
+            st = new StringTokenizer(S, "-", true); // 나누기
+            S = ""; // 초기화
+            while (st.hasMoreTokens()) {
+                String str = st.nextToken();
+                if (str.equals("-")) {  // "-" 이면 점수 추가
+                    ans += V;
+                } else {    // 아니면 S 문자열에 추가
+                    S += str;
+                }
+            }
         }
-        ans += sb.length();
+        ans += S.length();
+
         // output
         System.out.println(ans);
     }
 }
+
+/*
+input : 
+abcdabcd
+2
+abcd 8
+bcdabc 13
+
+output : 
+16
+
+정담 : 17점
+ */
