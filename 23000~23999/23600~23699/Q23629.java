9b1ebe2d289d1430e280a967e50c476cd7adcafa
KIMSEI <bbomi1973@gmail.com>
2022-06-30 12:49:59 +0900

    Q23629 failed

--- 23000~23999/23600~23699/Q23629.java
+++ 23000~23999/23600~23699/Q23629.java
@@ -6,65 +6,77 @@ public class Q23629 {
     static StringBuilder ans = new StringBuilder();
     static StringTokenizer st;
 
+    static String[] str_arr = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE" };
+    static int[] int_arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    static String S;
+    static Deque<String> deque;
+
     public static void main(String[] args) throws IOException {
         // input
-        String str = br.readLine();
-        String[] arr1 = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE" };
-        int[] arr2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        List<Long> lq = new ArrayList<>();
-        List<String> meq = new ArrayList<>();
-        // solve
-        for (int i = 0; i < arr1.length; i++) {
-            str = str.replaceAll(arr1[i], String.valueOf(arr2[i]));
+        S = br.readLine();
+        deque = new LinkedList<>();
+        for (int i = 0; i < str_arr.length; i++) { // 문자 -> 숫자 변환
+            S = S.replaceAll(str_arr[i], String.valueOf(int_arr[i]));
         }
-        ans.append(str).append("\n"); // 숫자로된 수식 저장
-        st = new StringTokenizer(str, "+-x/=", true);
-        int run = 0;
+
+        // solve
+        st = new StringTokenizer(S, "+-x/=", true); // 숫자와 연산자 분리
+
         while (st.hasMoreTokens()) {
-            if (run % 2 == 0) {
+            String str = st.nextToken();
+            if (str.equals("=")) { // 결과
+                while (deque.size() != 1) {
+                    try {
+                        int num_1 = Integer.parseInt(deque.pollFirst());
+                        String op_code = deque.pollFirst(); // 연산자
+                        int num_2 = Integer.parseInt(deque.pollFirst());
+                        int result_num; // 결과값
+                        if (op_code.endsWith("+")) { // 더하기
+                            result_num = num_1 + num_2;
+                        } else {
+                            result_num = num_1 - num_2;
+                        }
+                        deque.addLast(String.valueOf(result_num));
+                    } catch (Exception e) {
+                        ans.append("Madness!");
+                        break;
+                    }
+                }
+                if (ans.length() == 0) {
+                    ans.append(S + "\n");
+                    S = deque.pollLast();
+                    for (int i = 0; i < str_arr.length; i++) {
+                        S = S.replaceAll(String.valueOf(int_arr[i]), str_arr[i]);
+                    }
+                    ans.append(S);
+                }
+            } else if (str.equals("x") || str.equals("/")) { // 곱하기, 나누기
                 try {
-                    lq.add(Long.valueOf(st.nextToken()));
-                } catch (Exception e) { // 수식이 연속으로 나오면
-                    ans.delete(0, ans.length());
+                    int num_1 = Integer.parseInt(deque.pollLast());
+                    int num_2 = Integer.parseInt(st.nextToken());
+                    int result_num;
+                    if (str.equals("x")) { // 곱하기
+                        result_num = num_1 * num_2;
+                    } else { // 나누기
+                        result_num = num_1 / num_2;
+                        // 정수 나눗셈의 처리 방식은 C의 방식을 따른다.
+                        // 즉, 0에 가까운 정수를 취한다.
+                        if (num_1 % num_2 != 0 && deque.peekLast().equals("-")) {
+                            result_num += 1;
+                        }
+                    }
+                    deque.addLast(String.valueOf(result_num));
+                } catch (Exception e) {
                     ans.append("Madness!");
-                    run = 0;
                     break;
                 }
-            } else {
-                meq.add(st.nextToken());
+            } else { // 숫자, 더하기, 빼기
+                deque.addLast(str);
             }
-            run++;
         }
-        // 정상적으로 while문을 통과한다면
-        if (run != 0) {
-
-        }
-
-        // 소수점이하는 무조건 버림
 
         // output
-        System.out.println(lq);
-        System.out.println(meq);
         System.out.println(ans);
-    }
-
-    public static Long add() { // 더하기
-        Long add = 0L;
-        return add;
-    }
-
-    public static Long substract() { // 빼기
-        Long add = 0L;
-        return add;
-    }
-
-    public static Long multiply() { // 곱하기
-        Long add = 0L;
-        return add;
-    }
-
-    public static Long divide() { // 나누기
-        Long add = 0L;
-        return add;
     }
 }
