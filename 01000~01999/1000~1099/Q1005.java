d9b11237c4a33d4fa4dbb86cc3bc0f723136f911
KIMSEI <bbomi1973@gmail.com>
2022-07-03 14:54:47 +0900

    Q2178 solved

--- 01000~01999/1000~1099/Q1005.java
+++ 01000~01999/1000~1099/Q1005.java
@@ -5,13 +5,101 @@ public class Q1005 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder ans = new StringBuilder();
     static StringTokenizer st;
+    static int T; // 테스트케이스의 개수
+    static int N; // 건물의 개수
+    static int K; // 건물간의 건설순서 규칙의 총 개수
+    static int W; // 승리하기 위해 건설해야 할 건물의 번호
+
+    static int[] real_time; // 실제 건물이 건설되는 시간
+    static int[] craft_time; // 건물의 건설 시간
+    static boolean[] visited; // 방문 확인
+    static List<List<Integer>> cp_list; // 선행조건 리스트
+    static List<List<Integer>> build_list; // 건설할 수 있는 건물들
 
     public static void main(String[] args) throws IOException {
         // input
+        T = Integer.parseInt(br.readLine());
 
-        // solve
+        while (T > 0) {
+            // input
+            st = new StringTokenizer(br.readLine()); // 건물의 개수와 건설순서 규칙의 총 개수
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            real_time = new int[N + 1];
+            craft_time = new int[N + 1];
+            visited = new boolean[N + 1];
+            cp_list = new ArrayList<>();
+
+            st = new StringTokenizer(br.readLine()); // 각 건물당 건설에 걸리는 시간
+            for (int i = 0; i <= N; i++) {
+                cp_list.add(new ArrayList<>());
+                if (i != 0) {
+                    craft_time[i] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 1; i <= K; i++) { // 2차원 배열 설정
+                st = new StringTokenizer(br.readLine());
+                int cp = Integer.parseInt(st.nextToken()); // 선행 조건
+                int target = Integer.parseInt(st.nextToken()); // 짓는 건물
+                cp_list.get(target).add(cp);
+                build_list.get(cp).add(target);
+            }
+
+            W = Integer.parseInt(br.readLine()); // 건설해야할 건물 번호
+
+            // solve
+            /*
+             * 조건들
+             * 1) 시작이 1번 건물이 아닐수도 있다.
+             * 2) 선행 조건이 종료 조건일수도 있다.
+             * 2-1) 전체 탐색을 안하고도 종료 조건을 달성할수 있다.
+             */
+            Queue<Integer> queue = new LinkedList<>();
+            // 큐로 돌린다?
+            for (int i = 1; i <= N; i++) { // 조건1 해결
+                if (cp_list.get(i).size() == 0) {
+                    visited[i] = true;
+                    queue.add(i);
+                    real_time[i] = craft_time[i];
+                }
+            }
+
+            while (!queue.isEmpty()) {
+                int id = queue.poll();
+                for (Integer build_id : build_list.get(id)) {
+                    for (int i = 0; i < cp_list.get(build_id).size(); i++) {
+                        
+                    }
+                }
+                for (Integer build_id : build_list.get(id)) {
+                    queue.add(build_id);
+                }
+            }
+
+            System.out.println(queue);
+            System.out.println("craft_time" + Arrays.toString(craft_time));
+            System.out.println("real_time" + Arrays.toString(real_time));
+            ans.append(real_time[W] + "\n");
+            T--;
+        }
 
         // output
         System.out.println(ans);
+    }
+
+    /**
+     * 건설한 건물의 시간을 구한다.
+     * 
+     * @param id 건설할 건물의 번호
+     * @return 건설할 건물의 총 걸리는 시간
+     */
+    static public int max_craft_time(int id) {
+        int max_time = 0;
+        for (Integer start_id : cp_list.get(id)) {
+            max_time = Math.max(max_time, real_time[start_id]);
+        }
+        return max_time;
     }
 }
--- 02000~02999/2100~2199/Q2178.java
+++ 02000~02999/2100~2199/Q2178.java
@@ -3,92 +3,58 @@ import java.util.*;
 
 public class Q2178 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
     static StringTokenizer st;
+
+    static int[] dy = { -1, 0, 0, 1 };
+    static int[] dx = { 0, -1, 1, 0 };
 
     static int N, M;
     static char[][] maze;
-    static boolean[][] visted;
-    static List<Queue<pos<Integer, Integer>>> maze_pos;
+    static int[][] visited;
+    static Queue<int[]> queue = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         // input
-        int ans = 0;
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        maze = new char[N + 1][M + 1];
-        visted = new boolean[N + 1][M + 1];
-        maze_pos = new LinkedList<>();
-
+        maze = new char[N][M];
+        visited = new int[N][M];
         for (int i = 0; i < N; i++) {
             maze[i] = br.readLine().toCharArray();
-            // system.out.println("i: \"" + i + "\" > " + Arrays.toString(maze[i]) + "\"");
         }
 
         // solve
-        pos<Integer, Integer> pos = new pos<>();
-        boolean run = true;
-        while (run) {
-            maze_pos.add(new LinkedList<>());
-            if (maze_pos.size() == 0) { // 시작
-                pos.set_pos(0, 0);
-                visted[pos.get_y_pos()][pos.get_x_pos()] = true;
-                maze_pos.get(maze_pos.size() - 1).add(pos);
-            }
-            System.out.println(maze_pos.get(maze_pos.size()-1).size());
-            for (int i = 0; i < maze_pos.get(maze_pos.size() - 1).size(); i++) {
-                int x_pos = maze_pos.get(maze_pos.size() - 1).peek().get_x_pos();
-                int y_pos = maze_pos.get(maze_pos.size() - 1).poll().get_y_pos();
-                if (y_pos == N + 1 && x_pos == M + 1) {
-                    break;
+        // - setting
+        int[] pos = { 0, 0 }; // y, x
+        queue.add(pos);
+        visited[0][0] = 1;
+
+        // - bfs
+        while (!queue.isEmpty()) {
+            int[] now_pos = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int new_y = now_pos[0] + dy[i];
+                if (new_y < 0 || new_y >= N) { // y축 에러
+                    continue;
                 }
-                bfs(y_pos, x_pos);
+                int new_x = now_pos[1] + dx[i];
+                if (new_x < 0 || new_x >= M) { // x축 에러
+                    continue;
+                }
+                if (visited[new_y][new_x] != 0) { // 방문한 좌표
+                    continue;
+                }
+                if (maze[new_y][new_x] == '1') {
+                    int[] new_pos = { new_y, new_x };
+                    queue.add(new_pos);
+                    visited[new_y][new_x] = visited[now_pos[0]][now_pos[1]] + 1;
+                }
             }
         }
 
         // output
-        System.out.println(maze_pos.size());
-    }
-
-    static int[] x_pos = { 0, 0, -1, 1 };
-    static int[] y_pos = { -1, 1, 0, 0 };
-
-    static public void bfs(int y, int x) {
-        if (visted[y][x] == true) {
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            int x_calc = x + x_pos[i];
-            int y_calc = y + y_pos[i];
-            if (x_calc < 0 || x_calc > M)
-                continue;
-            if (y_calc < 0 || y_calc > N)
-                continue;
-            if (maze[y_calc][x_calc] == '1' && visted[y_calc][x_calc] == false) {
-                visted[y_calc][x_calc] = true;
-                pos<Integer, Integer> pos = new pos<>();
-                pos.set_pos(x_calc, y_calc);
-                maze_pos.get(maze_pos.size() - 1).add(pos);
-            }
-        }
-    }
-}
-
-class pos<x, y> {
-    private int x;
-    private int y;
-
-    public void set_pos(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int get_x_pos() {
-        return this.x;
-    }
-
-    public int get_y_pos() {
-        return this.y;
+        System.out.println(visited[N - 1][M - 1]);
     }
 }
