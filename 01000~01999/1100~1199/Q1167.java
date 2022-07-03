011cf95395de515b845e5b6b819088b108b19a76
KIMSEI <bbomi1973@gmail.com>
2022-07-03 14:59:26 +0900

    Q1167 solving, README.md Polishing

---
+++ 01000~01999/1100~1199/Q1167.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Q1167 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        // input
+
+        // solve
+
+        // output
+        System.out.println(ans);
+    }
+}
