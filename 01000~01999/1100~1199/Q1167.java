529128029963c5de3531120dbdc5f72aa4b63f92
KIMSEI <bbomi1973@gmail.com>
2022-07-04 15:46:35 +0900

    Q9080 solving

--- 01000~01999/1100~1199/Q1167.java
+++ 01000~01999/1100~1199/Q1167.java
@@ -6,11 +6,47 @@ public class Q1167 {
     static StringBuilder ans = new StringBuilder();
     static StringTokenizer st;
 
+    static int V; // 트리의 정점의 개수
+    static boolean[] visited;
+    static Queue<Integer> queue = new LinkedList<>(); // 좌표값만 저장
+    static int[] length;
+    static List<List<Map<Integer, Integer>>> tree = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         // input
+        V = Integer.parseInt(br.readLine());
+        visited = new boolean[V + 1];
+        length = new int[V + 1];
+
+        for (int i = 0; i < V; i++) {
+            tree.add(new ArrayList<>());
+            st = new StringTokenizer(br.readLine());
+            int id = Integer.parseInt(st.nextToken()); // 정점의 Id
+            while (st.hasMoreTokens()) {
+                int node = Integer.parseInt(st.nextToken());
+                if (node != -1) {
+                    int edge = Integer.parseInt(st.nextToken());
+                    Map<Integer, Integer> node_map = new HashMap<>();
+                    node_map.put(node, edge); // 정점, 간선의 거리
+                    tree.get(id - 1).add(node_map);
+                }
+            }
+        }
 
         // solve
+        queue.add(1);
+        while (!queue.isEmpty()) {
+            int pos = queue.poll();
+            visited[pos] = true; // 방문확인
+            for (Map<Integer, Integer> node : tree.get(pos)) {
+                // int node_pos = node.get(key)
+                // HashMap으로 하니 Key값을 알수가 없다.
+                // id도 따로 저장을 해둬야 하는가?... -- 메모리 증가
+                // 아니면 다른 방식이 있을까?
+                // 객체를 만들어 두고 for문을 돌린다?... 
+            }
 
+        }
         // output
         System.out.println(ans);
     }
---
+++ 09000~09999/9000~9099/Q9080.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Q9080 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
+    static StringTokenizer st;
+
+    static int T;
+    static int[][] day_hour_min;
+
+    public static void main(String[] args) throws IOException {
+        // input
+        T = Integer.parseInt(br.readLine());
+
+        while (T > 0) {
+            input();
+            // solve
+            // 22:00 - 8 : 00 야간 요금 이용 가능 -> 5,000원
+            // - 야간 요금은 241분 이상 즐기면 이득 그외에는 일반 요금이 이득
+            T--;
+        }
+
+        // output
+        System.out.println(ans);
+    }
+
+    public static void input() throws IOException {
+        day_hour_min = new int[2][3]; // 일, 시, 분
+        st = new StringTokenizer(br.readLine());
+        String time_str = st.nextToken();
+        int play_time = Integer.parseInt(st.nextToken()); // 이용 시간
+        st = new StringTokenizer(time_str, ":"); // hour, min 분리
+        day_hour_min[0][1] = Integer.parseInt(st.nextToken());
+        day_hour_min[0][2] = Integer.parseInt(st.nextToken());
+
+        day_hour_min[1][1] = day_hour_min[0][1] + play_time / 60;
+        day_hour_min[1][2] = day_hour_min[0][2] + play_time % 60;
+        if (day_hour_min[1][2] >= 60) { // min 계산기
+            day_hour_min[1][2] -= 60;
+            day_hour_min[1][1] += 1;
+        }
+        if (day_hour_min[1][1] >= 24) { // hour 계산기
+            day_hour_min[1][1] -= 24;
+            day_hour_min[1][0] += 1;
+        }
+    }
+}
