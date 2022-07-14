54fe722c9b9d3c8f33eb3c97529ce30a50fdae27
KIMSEI <bbomi1973@gmail.com>
2022-07-14 16:47:49 +0900

    Q1003 sovled

--- 01000~01999/1000~1099/Q1003.java
+++ 01000~01999/1000~1099/Q1003.java
@@ -1,36 +1,47 @@
 import java.io.*;
 import java.util.*;
 
-public class Q1003 {    // 시간 초과 배열을 생성하여 처리하기.
-    static Scanner sc = new Scanner(System.in);
-    static int zeroCount;
-    static int oneCount;
+public class Q1003 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
+    static StringTokenizer st;
+    static int T, N;
+    static int[][] DP;
 
-    public static void main(String args[]) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        // input
+        T = Integer.parseInt(br.readLine()); // 테스트 케이스의 수
+        DP = new int[2][41];
+        DP[0][0] = 1;
+        DP[1][1] = 1;
+
+        // solve
+        /* 1. 피보나치 함수 DP 구하기 */
+        for (int i = 2; i <= 40; i++) {
+            DP[0][i] = DP[0][i - 1] + DP[0][i - 2];
+            DP[1][i] = DP[1][i - 1] + DP[1][i - 2];
+        }
+
+        /* 정답 입력 */
         while (T > 0) {
-            st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            zeroCount = 0;
-            oneCount = 0;
-            fibonacci(N);
-            sb.append(zeroCount + " " + oneCount + "\n");
+            N = Integer.parseInt(br.readLine());
+            ans.append(DP[0][N] + " " + DP[1][N] + "\n");
             T--;
         }
-        System.out.println(sb);
+
+        // output
+        System.out.println(ans);
     }
 
-    public static void fibonacci(int N) {
-        if (N == 0) {
-            zeroCount++;
-        } else if (N == 1) {
-            oneCount++;
+    public static int f(int n) {
+        if (n == 0) {
+            System.out.println("0");
+            return 0;
+        } else if (n == 1) {
+            System.out.println("1");
+            return 1;
         } else {
-            fibonacci(N - 1);
-            fibonacci(N - 2);
+            return f(n - 1) + f(n - 2);
         }
     }
 }
