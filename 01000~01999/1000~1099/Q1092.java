18b86e695be626b1fb6914d897d91fa699840187
KIMSEI <bbomi1973@gmail.com>
2022-07-19 12:35:25 +0900

    Q1092 failed

--- 01000~01999/1000~1099/Q1092.java
+++ 01000~01999/1000~1099/Q1092.java
@@ -3,43 +3,67 @@ import java.util.*;
 
 public class Q1092 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
     static StringTokenizer st;
+    static int N, M;
 
-    public static void main(String[] args) throws IOException { // 시간 초과
-        int sec = 0;
-        // 크래인 배열 생성하기
-        int crane = Integer.parseInt(br.readLine());
-        int[] craneArr = new int[crane];
-        craneArr = input(craneArr);
-        // 박스 배열 생성하기
-        int box = Integer.parseInt(br.readLine());
-        int[] boxArr = new int[box];
-        boolean[] boxIsArr = new boolean[box];
-        boxArr = input(boxArr);
-        // 배열 정렬하기
-        // Arrays.sort(craneArr);
-        Arrays.sort(boxArr);
-        // 풀이
-        while (box > 0) {
-            for (int i = craneArr.length - 1; i >= 0; i--) {
-                for (int j = boxArr.length - 1; j >= 0; j--) {
-                    if (craneArr[i] >= boxArr[j] && boxIsArr[j] == false) {
-                        box--;
-                        boxIsArr[j] = true;
-                        break;
+    static List<Integer> C;
+    static HashMap<Integer, Integer> B; // Key : 박스의 무게, Value : 박스의 개수
+
+    public static void main(String[] args) throws IOException {
+        // input
+        int max_C = 0, max_B = 0, min_B = 1000000, day = 0;
+        N = Integer.parseInt(br.readLine()); // 크레인의 수
+        C = new ArrayList<>(); // 크레인의 무게 제한
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            C.add(Integer.parseInt(st.nextToken()));
+            max_C = Math.max(max_C, C.get(C.size() - 1));
+        }
+        M = Integer.parseInt(br.readLine()); // 박스의 수
+        B = new HashMap<>(); // 박스들의 집합
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            int b = Integer.parseInt(st.nextToken()); // 박스의 무개
+            max_B = Math.max(max_B, b);
+            min_B = Math.min(min_B, b);
+            if (!B.containsKey(b)) {
+                B.put(b, 1);
+            } else {
+                B.put(b, B.get(b) + 1);
+            }
+        }
+
+        // solve
+        if (max_B > max_C) {
+            ans.append("-1");
+        } else {
+            /* 1. 크레인은 자신이 옴길수 있는 가장 큰 박스를 먼저 움긴다. */
+            int c_power;
+            int b_count;
+            while (M > 0) {
+                for (int i = 0; i < C.size(); i++) {
+                    c_power = C.get(i);
+                    for (int j = c_power; j >= min_B; j--) {
+                        if (B.containsKey(j)) {
+                            b_count = B.get(j);
+                            if (b_count > 0) {
+                                // System.out.println(B);
+                                B.replace(j, b_count - 1);
+                                M--;
+                                break;
+                            }
+                        } else {
+                            continue;
+                        }
                     }
                 }
+                day++;
             }
-            sec++;
+            ans.append(day);
         }
-        System.out.println(sec);
-    }
 
-    public static int[] input(int[] arr) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        return arr;
+        // output
+        System.out.println(ans);
     }
 }
