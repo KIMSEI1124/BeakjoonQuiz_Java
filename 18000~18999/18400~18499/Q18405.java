d87014ba2acae4b1ad9caef245a9ddaeb9f1bf7b
KIMSEI <bbomi1973@gmail.com>
2022-07-19 15:44:05 +0900

    Q18405 solving

---
+++ 18000~18999/18400~18499/Q18405.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Q18405 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        // input
+
+        // solve
+
+        // output
+        System.out.println(ans);
+    }
+}
