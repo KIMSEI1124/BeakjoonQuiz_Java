3bd845d9bc5b488f57245cf3c1a0f14edead1352
KIMSEI <bbomi1973@gmail.com>
2022-07-21 12:59:26 +0900

    Q1010 solved

--- 01000~01999/1000~1099/Q1010.java
+++ 01000~01999/1000~1099/Q1010.java
@@ -1,24 +1,37 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Q1010 {
-    static Scanner sc = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
-    static final int testCaseCount = sc.nextInt(); // 테스트 케이스의 개수
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
+    static StringTokenizer st;
+    static int T, N, M;
+    static int[][] DP = new int[30][30];
 
-    public static void main(String[] args) {
-        for (int i = 0; i < testCaseCount; i++) {
-            sb.append(String.valueOf(solve()) + "\n");
-        }
-        System.out.println(sb);
-    }
+    public static void main(String[] args) throws IOException {
+        // input
+        T = Integer.parseInt(br.readLine()); // 테스트 케이스의 개수
 
-    public static int solve() {
-        int answer = 0;
-        int east_Point = sc.nextInt(); // 강 서쪽
-        int west_Point = sc.nextInt(); // 강 동쪽
-        for (int i = 0; i < 100; i++) {
-            
+        // solve
+        for (int i = 1; i < 30; i++) {
+            for (int j = i; j < 30; j++) {
+                if (i == 1) {
+                    DP[i][j] = j;
+                } else {
+                    DP[i][j] = DP[i][j - 1] + DP[i - 1][j - 1];
+                }
+            }
         }
-        return answer;
+
+        while (T > 0) {
+            T--;
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken()); // 강 서쪽
+            M = Integer.parseInt(st.nextToken()); // 강 동쪽
+            ans.append(DP[N][M]).append("\n");
+        }
+
+        // output
+        System.out.println(ans);
     }
 }
