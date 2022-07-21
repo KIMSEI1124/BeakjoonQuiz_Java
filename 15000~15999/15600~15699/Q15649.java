88a4009d451ede0b9456bffadb09c5dda8f8bb8a
KIMSEI <bbomi1973@gmail.com>
2022-07-21 14:02:43 +0900

    Q15649 Solved

---
+++ 15000~15999/15600~15699/Q15649.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Q15649 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
+    static StringTokenizer st;
+    static int N, M;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        // input
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 1 ~ N까지 자연수
+        M = Integer.parseInt(st.nextToken()); // M개를 고른 수열
+
+        // solve
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N + 1];
+            dfs(i, 0, "");
+        }
+
+        // output
+        System.out.println(ans);
+    }
+
+    public static void dfs(int id, int depth, String str) {
+        if (visited[id] || depth > M) {
+            return;
+        }
+        visited[id] = true;
+        str += id + " ";
+        depth++;
+        if (depth == M) {
+            ans.append(str + "\n");
+        }
+        for (int i = 1; i <= N; i++) {
+            dfs(i, depth, str);
+        }
+        visited[id] = false;
+    }
+}
