ff903dd2b2d856ba6c869701e52d159764d3fee0
KIMSEI <bbomi1973@gmail.com>
2022-07-26 22:54:49 +0900

    Q25214 create

---
+++ 25000~25999/25200~25299/Q25214.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class class_name {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder ans = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        // input
+
+        // solve
+
+        // output
+        System.out.println(ans);
+    }
+}
