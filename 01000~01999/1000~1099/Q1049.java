e1aefa18d43b57ed62b4f649aa69d938a32124ad
Jeonguk Kim <74192619+KIMSEI1124@users.noreply.github.com>
2022-10-23 16:24:46 +0900

    1049 solved

--- 01000~01999/1000~1099/Q1049.java
+++ 01000~01999/1000~1099/Q1049.java
@@ -5,36 +5,29 @@ public class Q1049 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 구매 해야하는 기타줄의 수
-        int M = Integer.parseInt(st.nextToken()); // 브랜드의 수
-        var six = 1000; // 기타줄 패키지로 구매
-        int one = 1000; // 기타줄 단품으로 구매
-        int result = 0;
-
-        for (int i = 0; i < M; i++) {
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int package_price = 1000;
+        int one_price = 1000;
+        int result;
+        // 1
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            six = Math.min(six, Integer.parseInt(st.nextToken()));
-            one = Math.min(one, Integer.parseInt(st.nextToken()));
+            package_price = Math.min(package_price, Integer.parseInt(st.nextToken()));
+            one_price = Math.min(one_price, Integer.parseInt(st.nextToken()));
         }
 
-        /*
-         * Solve
-         * 단품 6개의 가격이 패키지보다 저렴하면 -> 단품으로 전부 구입
-         * 패키지가 단품보다 저렴하면 -> 패키지로 전부 구입
-         * 그 외 -> 패키지와 단품으로 구입
-         */
-        if (six > one * 6) {
-            result = one * N;
-        } else if (six / 6 < one) {
-            result = N / 6 * six;
+        // 2
+        if(package_price <= one_price * (N % 6)) {
+            result = N / 6 * package_price;
             if(N % 6 != 0) {
-                result += six;
+                result += package_price;
             }
+        } else if (package_price > one_price * 6) {
+            result = N * one_price;
         } else {
-            result = ( N / 6 ) * six + N % 6 * one;
+            result = N / 6 * package_price + N % 6 * one_price;
         }
-
         System.out.println(result);
     }
 }
