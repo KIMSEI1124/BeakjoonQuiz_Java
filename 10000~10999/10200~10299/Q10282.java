22f47580038ab1426884ad7e384d4d2dfb24e0b3
KIMSEI <bbomi1973@gmail.com>
2022-11-11 16:30:40 +0900

    docs: .gitignore 수정

--- 10000~10999/10200~10299/Q10282.java
+++ 10000~10999/10200~10299/Q10282.java
@@ -5,55 +5,74 @@ public class Q10282 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder ans = new StringBuilder();
     static StringTokenizer st;
-    static int T, n, d, c;
-    static List<List<Computer_10282<Integer, Integer>>> net;
 
     public static void main(String[] args) throws IOException {
         // input
-        T = Integer.parseInt(br.readLine());
-        int a, b, sec;
+        int T = Integer.parseInt(br.readLine());
         while (T > 0) {
             st = new StringTokenizer(br.readLine());
-            net = new ArrayList<>();
-            n = Integer.parseInt(st.nextToken()); // 컴퓨터의 개수
-            d = Integer.parseInt(st.nextToken()); // 의존성의 개수
-            c = Integer.parseInt(st.nextToken()); // 해킹당할 컴퓨터의 번호
-            for (int i = 0; i < n; i++) {
-                net.add(new ArrayList<>());
-            }
-            for (int i = 0; i < d; i++) {
-                st = new StringTokenizer(br.readLine());
-                a = Integer.parseInt(st.nextToken());   // a 컴퓨터가
-                b = Integer.parseInt(st.nextToken());   // b 를 의존
-                sec = Integer.parseInt(st.nextToken()); // 감염되기까지 걸리는 시간
-                Computer_10282<Integer, Integer> com = new Computer_10282<>();
-                com.set_computer(a, sec);
-                net.get(b).add(com);
-            }
-            // solve
-
+            int n = Integer.parseInt(st.nextToken()); // 컴퓨터의 개수
+            int d = Integer.parseInt(st.nextToken()); // 의존성의 개수
+            int c = Integer.parseInt(st.nextToken()); // 해킹당한 컴퓨터의 번호
+            List<List<HashMap<Integer,Integer>>> com = set_com(n, d);
+            hack(com, c);
             T--;
         }
 
         // output
         System.out.println(ans);
     }
-}
 
-class Computer_10282<id, sec> {
-    private int id;
-    private int sec;
-
-    public void set_computer(int id, int sec) {
-        this.id = id;
-        this.sec = sec;
+    /**
+     * 컴퓨터를 설정한다.
+     * @param n 컴퓨터의 개수
+     * @param d 의존성의 개수
+     * @return  세팅된 컴퓨터의 정보
+     * @throws IOException
+     */
+    public static List<List<HashMap<Integer,Integer>>> set_com(int n, int d) throws IOException {
+        List<List<HashMap<Integer,Integer>>> com = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            com.add(new ArrayList<>());
+        }
+        for (int i = 0; i < d; i++) {
+            com = set_edge(com);
+        }
+        return com;
     }
 
-    public int get_id() {
-        return this.id;
+    /**
+     * 컴퓨터의 의존성을 추가한다.
+     * @param com 비어있는 컴퓨터 정보
+     * @return  세팅된 컴퓨터 정보
+     * @throws IOException
+     */
+    public static List<List<HashMap<Integer,Integer>>> set_edge(List<List<HashMap<Integer,Integer>>> com) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken()); // a 컴퓨터가
+        int b = Integer.parseInt(st.nextToken()); // b 컴퓨터를 의존
+        int s = Integer.parseInt(st.nextToken()); // s초 후 컴퓨터 a 감염
+
+        //HashMap 생성후 데이터 입력
+        com.get(b).add(new HashMap<>());
+        com.get(b).get(com.get(b).size()-1).put(a, s);
+        System.out.println(com.get(b).get(com.get(b).size()-1).values());
+        return com;
     }
 
-    public int get_sec() {
-        return this.sec;
+    public static void hack(List<List<HashMap<Integer,Integer>>> com, int c) {
+        boolean[] visited = new boolean[com.size()];
+        /*
+         * t_1 + t_2 < t_3 이면 t_1 + t_2 로 교체
+         */
+        int[] timer = new int[com.size()];
+        dfs(c, visited);
+    }
+
+    public static void dfs(int i, boolean[] visited) {
+        if(visited[i]) {
+            return;
+        }
+        visited[i] = true;
     }
 }
