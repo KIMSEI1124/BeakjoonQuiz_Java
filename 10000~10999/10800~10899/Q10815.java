973d17d645814a58c9ea9d797ad03629d751c5bf
KIMSEI1124 <workju1124@gmail.com>
2022-11-26 21:17:59 +0900

    solved: Q10815

---
+++ 10000~10999/10800~10899/Q10815.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Q10815 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder answer = new StringBuilder();
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> setN = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).boxed()
+                .collect(Collectors.toSet());
+        int M = Integer.parseInt(br.readLine());
+        
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            int number = Integer.parseInt(st.nextToken());
+            if(setN.contains(number)) {
+                answer.append("1 ");
+                continue;
+            }
+            answer.append("0 ");
+        }
+
+        System.out.println(answer);
+    }
+}
