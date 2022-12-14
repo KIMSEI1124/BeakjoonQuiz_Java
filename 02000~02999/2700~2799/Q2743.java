de27384485a7589930b745e84f3b34053cc752de
KIMSEI1124 <workju1124@gmail.com>
2022-12-14 11:45:49 +0900

    solved: Q2743

---
+++ 02000~02999/2700~2799/Q2743.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Q2743 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(br.readLine().length());
+    }
+}
