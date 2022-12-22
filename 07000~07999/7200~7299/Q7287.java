e51e4b0437dad8c0da5e5e0ce6b8bd88f5e764f3
KIMSEI1124 <workju1124@gmail.com>
2022-12-22 11:46:37 +0900

    solved: Q7287

---
+++ 07000~07999/7200~7299/Q7287.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Q7287 {
+    private static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int quantity = 183; //22.12.22
+        answer.append(quantity).append("\n").append("bbomi1973");
+        System.out.println(answer);
+    }
+}
