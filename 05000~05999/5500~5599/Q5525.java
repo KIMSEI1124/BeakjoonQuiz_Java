4ba02e916a56c3798a78e5b10747a15d32d8eeee
KIMSEI1124 <workju1124@gmail.com>
2023-02-17 21:32:13 +0900

    half Solved: Q5525

--- 05000~05999/5500~5599/Q5525.java
+++ 05000~05999/5500~5599/Q5525.java
@@ -6,21 +6,43 @@ public class Q5525 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()); // O의 개수
-        int M = Integer.parseInt(br.readLine()); // 문자열의 길이
-        String S = br.readLine();
-        StringBuilder check = new StringBuilder("I");
-        int answer = 0;
-        while (N > 0) {
-            check.append("OI");
-            N--;
+        int M = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        String IOI = getIOI(N);
+        System.out.println(getResult(IOI, str));
+    }
+
+    private static String getIOI(int N) {
+        StringBuilder sb = new StringBuilder("I");
+        while (N-- > 0) {
+            sb.append("OI");
         }
-        int size = check.length();
-        String str = String.valueOf(check);
-        for (int i = 0; i < M - size; i++) {
-            if (S.subSequence(i, i + size).equals(str)) {
-                answer++;
+        return String.valueOf(sb);
+    }
+
+    private static int getResult(String IOI, String str) {
+        int result = 0;
+        for (int i = 0; i < str.length() - IOI.length() + 1; i++) {
+            String temp = str.substring(i, i + IOI.length());
+            if (IOI.equals(temp)) {
+                result++;
+                // int count = getCount(i, IOI, str);
+                // i += (count * 2) - 1;
             }
         }
-        System.out.println(answer);
+        return result;
+    }
+
+    // TODO : 시간복잡도 문제 해결중
+    private static int getCount(int i, String IOI, String str) {
+        int count = 0;
+        for (int j = i + IOI.length(); j < str.length() - IOI.length(); j++) {
+            String temp = str.substring(i, i+2);
+            if(!temp.equals("OI")) {
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
