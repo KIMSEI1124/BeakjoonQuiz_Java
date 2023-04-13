86cdeb1cc6f397d42dfe8b833791c37adb74fee2
KIMSEI1124 <workju1124@gmail.com>
2023-04-13 10:42:21 +0900

    solved: 짝수의 합

---
+++ Programmers/Java/src/LEVEL1/문자열_나누기.java
@@ -0,0 +1,30 @@
+package LEVEL1;
+
+/**
+ * 풀이 시간 : 7분
+ */
+public class 문자열_나누기 {
+    public int solution(String s) {
+        int answer = 0;
+        char temp = ' ';
+        int[] x = new int[2];   // [0] : same, [1] : different
+        char[] array = s.toCharArray();
+        for (char c : array) {
+            if (x[0] == 0) {
+                x[0]++;
+                answer++;
+                temp = c;
+                continue;
+            }
+            if (temp == c) {
+                x[0]++;
+            } else {
+                x[1]++;
+            }
+            if (x[0] == x[1]) {
+                x = new int[2];
+            }
+        }
+        return answer;
+    }
+}
