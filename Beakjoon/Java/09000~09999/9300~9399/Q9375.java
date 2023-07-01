ce529f51279af8ae58b9bdb7d2b2d7a9edf65a44
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2023-07-01 20:38:12 +0900

    faild: Q9375

--- Beakjoon/Java/09000~09999/9300~9399/Q9375.java
+++ Beakjoon/Java/09000~09999/9300~9399/Q9375.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Q9375 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringBuilder answer = new StringBuilder();
-    private static StringTokenizer st;
 
     private static int T;
     private static int N;
@@ -14,9 +13,37 @@ public class Q9375 {
 
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
-            for (int i = 0; i < N; i++) {
 
+            if (N == 0) {
+                answer.append("0\n");
+                continue;
             }
+
+            int count = 1;
+            Map<String, Integer> map = new HashMap<>();
+            List<String> keys = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                String[] item = br.readLine().split(" ");
+                if (map.containsKey(item[1])) {
+                    map.put(item[1], map.get(item[1]) + 1);
+                    continue;
+                }
+                map.put(item[1], 1);
+                keys.add(item[1]);
+            }
+
+            if (keys.size() == 1) {
+                answer.append(N).append("\n");
+                continue;
+            }
+
+            for (String key : keys) {
+                count *= map.get(key);
+            }
+            count += N;
+            answer.append(count).append("\n");
         }
+
+        System.out.println(answer);
     }
 }
