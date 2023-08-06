722ea958c4592d8d5ac834c8fc60bf60cc19982e
KIMSEI1124 <workju1124@gmail.com>
2023-08-06 19:19:20 +0900

    Solved: Q1182

--- Beakjoon/Java/01000~01999/1100~1199/Q1182.java
+++ Beakjoon/Java/01000~01999/1100~1199/Q1182.java
@@ -2,31 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Q1182 {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
+    private static int answer;
+
+    private static int N, S;
+    private static int[] array;
 
     public static void main(String[] args) throws IOException {
-        int answer = 0;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
+        input();
+        solve(0, 0, 0);
+        System.out.println(answer);
+    }
 
-        int[] array = new int[n];
+    private static void input() throws IOException {
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        array = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
+    }
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += array[j];
-                if (sum == s) {
-                    answer++;
-                }
-            }
+    private static void solve(int idx, int length, int sum) {
+        if (idx + length >= N) {
+            return;
         }
 
-        System.out.println(answer);
+        sum += array[idx + length];
+        solve(idx, length + 1, sum);
+
+        if (S == sum) {
+            answer++;
+        }
+
+        sum -= array[idx + length];
+        solve(idx + 1, length, sum);
     }
 }
