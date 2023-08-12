8f53b6b48386cffea1666978bd94a051f78df2c3
KIMSEI1124 <workju1124@gmail.com>
2023-08-12 11:52:54 +0900

    Study: 숫자 비트 판별 테스트 작성

--- Study/java/src/bit/숫자_비트_판별.java
+++ Study/java/src/bit/숫자_비트_판별.java
@@ -1,11 +1,9 @@
 package bit;
 
-public class 배열_비트_중복 {
+public class 숫자_비트_판별 {
     public static void main(String[] args) {
-        int[] a = {1, 5, 9, 10};
-        int[] b = {5, 10, 13, 20};
-
-        int bitA = 0;
-        int bitB = 0;
+        int number = 5;
+        int bitCount = Integer.bitCount(number);
+        System.out.println(bitCount);
     }
 }
