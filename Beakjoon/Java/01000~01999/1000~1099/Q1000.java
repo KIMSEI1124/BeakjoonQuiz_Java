223ed580a54a5a5c822c17d8f2aed0f0c60348b0
KIMSEI1124 <workju1124@gmail.com>
2024-01-27 22:32:57 +0900

    Remove: 필요없는 파일 삭제

--- Beakjoon/Java/01000~01999/1000~1099/Q1000.java
+++ Beakjoon/Java/01000~01999/1000~1099/Q1000.java
@@ -2,14 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Q1000 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 
 	public static void main(String[] args) throws IOException {
 		// input
-        st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
 		// solve
 		int ans = A + B;
 		// output
