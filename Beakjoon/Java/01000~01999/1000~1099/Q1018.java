0f2afeddc19acb4195f5c6e40c0cd44174a1c286
KIMSEI1124 <workju1124@gmail.com>
2024-03-03 15:06:39 +0900

    Failed(Java): Q1018

--- Beakjoon/Java/01000~01999/1000~1099/Q1018.java
+++ Beakjoon/Java/01000~01999/1000~1099/Q1018.java
@@ -2,51 +2,81 @@ import java.io.*;
 import java.util.*;
 
 public class Q1018 {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static StringTokenizer st;
-    private static int n, m;
-    private static int[][] grid;
+    private static int answer = Integer.MAX_VALUE;
+
+    private static int N;
+    private static int M;
+    private static boolean[][] grid;
 
     public static void main(String[] args) throws IOException {
-        /* Input */
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        grid = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            char[] c = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                grid[i][j] = inputElement(c[j]);
-            }
-            System.out.println(Arrays.toString(grid[i]));
-        }
-
-        /* Solve */
-        int zero = getCount(0);
-        int one = getCount(1);
-        System.out.println(zero);
-        System.out.println(one);
-        int answer = Math.min(zero, one);
+        input();
+        solve();
         System.out.println(answer);
     }
 
-    private static int inputElement(char c) {
-        if (c == 'W') {
-            return 0;
-        }
-        return 1;
-    }
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-    private static int getCount(int type) {
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int temp = grid[i][j] ^ type;
-                if (temp != grid[i][j]) {
-                    count++;
-                }
+        grid = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            char[] temp = br.readLine().toCharArray();
+            for (int j = 0; j < M; j++) {
+                grid[i][j] = temp[j] == 'W';
             }
         }
-        return count;
+    }
+
+    private static void solve() {
+        for (int i = 0; i < N - 7; i++) {
+            for (int j = 0; j < M - 7; j++) {
+                answer = Math.min(check(i, j), answer);
+            }
+        }
+    }
+
+    private static int check(int y, int x) {
+        boolean[][] map = copyMap(y, x);
+        boolean isWhite = map[0][0];
+        boolean isBlack = map[0][0];
+        int whiteCount = 0;
+        int blackCount = 0;
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                if (isWhite && c % 2 == 0 && map[r][c] == false) {
+                    whiteCount++;
+                } else if (isWhite && c % 2 == 1 && map[r][c]) {
+                    whiteCount++;
+                } else if (!isWhite && c % 2 == 0 && map[r][c]) {
+                    whiteCount++;
+                } else if (!isWhite && c % 2 == 1 && map[r][c] == false) {
+                    whiteCount++;
+                }
+                if (isBlack && c % 2 == 0 && map[r][c]) {
+                    blackCount++;
+                } else if (isBlack && c % 2 == 1 && map[r][c] == false) {
+                    blackCount++;
+                } else if (!isBlack && c % 2 == 1 && map[r][c] == false) {
+                    blackCount++;
+                } else if (!isBlack && c % 2 == 0 && map[r][c]) {
+                    blackCount++;
+                }
+            }
+            isWhite = !isWhite;
+            isBlack = !isBlack;
+        }
+        return Math.min(whiteCount, blackCount);
+    }
+
+    private static boolean[][] copyMap(int y, int x) {
+        boolean[][] map = new boolean[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                map[i][j] = grid[i + y][j + x];
+            }
+        }
+        return map;
     }
 }
